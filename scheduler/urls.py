4e03a28a2ac37c5d8d4c6d5772e55ce9580a4521
nikolaj <strashevskiynn@g,ail.com>
2021-04-17 07:15:39 +0300

    мелкие доделки

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,7 +4,6 @@ from schedulerZstatic.views import home
 
 urlpatterns = [
     path('about/', home, name='home'),
-    path('blog/', include('schedulerZ.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include('schedulerZ.urls', namespace='api')),
     path('api-auth/', include('rest_framework.urls')),
